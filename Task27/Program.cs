22c69eb92be3f9d4f09f202a8fedc78ff0125324
Vladislav <126780307+galdinsh@users.noreply.github.com>
2023-03-18 15:49:06 +0500

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = 0; 
+for(int m = 1; m > 0; n /=10)
+{
+    m = n % 10;
+    s = s + m;
+}
+Console.WriteLine($"Результат = {s}");
