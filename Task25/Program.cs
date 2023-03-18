c5f0b605a4d7aca43f0b53b981f3cbf4444e9b57
Vladislav <126780307+galdinsh@users.noreply.github.com>
2023-03-18 14:32:10 +0500

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int s = 1;
+for(int i = 1; i <= m; i++)
+{
+s = s * n;
+}
+Console.WriteLine($"Результат = {s}");
