1191540340656f58fa39f23dd94d92a06ffa2f78
Vladislav <126780307+galdinsh@users.noreply.github.com>
2023-03-20 16:48:19 +0500

    Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -8,3 +8,11 @@ for(int m = 1; m > 0; n /=10)
     s = s + m;
 }
 Console.WriteLine($"Результат = {s}");
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// string s = Console.ReadLine()!;
+// int sum = 0;
+// for (int i = 0; i < s.Length; i++)
+//     sum = sum + int.Parse(s[i].ToString());
+// Console.WriteLine($"сумма цифр числа- {sum}");
